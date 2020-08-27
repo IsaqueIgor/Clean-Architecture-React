413bc6dac50a651bb284c398c01dffdb7fe3260a
Isaque Igor da Silva <isaqueigorsilva@gmail.com>
2020-08-27 13:10:13 -0300

    test: ensure RemoteAuthentication  returns an Account on Success

--- src/data/userCases/authentication/remote-authentication.spec.ts
+++ src/data/userCases/authentication/remote-authentication.spec.ts
@@ -1,6 +1,9 @@
 import faker from 'faker';
 
-import { mockAuthentication } from '@/domain/test/mock-authentication';
+import {
+  mockAuthentication,
+  mockAccountModel,
+} from '@/domain/test/mock-account';
 import { HttpPostClientSpy } from '@/data/test/mock-http-client';
 import { InvalidCredentialsError } from '@/domain/errors/invalid-credentials-error';
 import { HttpStatusCode } from '@/data/protocols/http/http-response';
@@ -75,5 +78,16 @@ describe('RemoteAuthentication', () => {
     };
     const promise = sut.auth(mockAuthentication());
     expect(promise).rejects.toThrow(new UnexpectedError());
+  });
+
+  test('should return an AccountModel if HttpPostClient returns 200', async () => {
+    const { sut, httpPostClientSpy, } = makeSut();
+    const httpResult = mockAccountModel();
+    httpPostClientSpy.response = {
+      statusCode: HttpStatusCode.ok,
+      body: httpResult,
+    };
+    const account = await sut.auth(mockAuthentication());
+    expect(account).toEqual(httpResult);
   });
 });
--- src/domain/test/mock-account.ts
+++ src/domain/test/mock-account.ts
@@ -1,7 +1,12 @@
 import faker from 'faker';
 import { AuthenticationParams } from '@/domain/userCases/autentication';
+import { AccountModel } from '../models/account-model';
 
 export const mockAuthentication = (): AuthenticationParams => ({
   email: faker.internet.email(),
   password: faker.internet.password(),
+});
+
+export const mockAccountModel = (): AccountModel => ({
+  accessToken: faker.random.uuid(),
 });
