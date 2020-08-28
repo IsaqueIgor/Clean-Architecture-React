01793fae9fb6b8ae92b2add1d5868a4c58e79e53
Isaque Igor da Silva <isaqueigorsilva@gmail.com>
2020-08-27 22:19:37 -0300

    feat: ensure AxiosHttpClient calls axios with correct URL

---
+++ src/infra/http/axios-http-client/axios-http-client.ts
@@ -0,0 +1,10 @@
+/* eslint-disable class-methods-use-this */
+import axios from 'axios';
+
+import { HttpPostParams } from '@/data/protocols/http';
+
+export class AxiosHttpClient {
+  async post(params: HttpPostParams<any>): Promise<void> {
+    await axios(params.url);
+  }
+}
