6e4ffc891c4619601522bbc3fca0d6c5432342c2
Isaque Igor da Silva <isaqueigorsilva@gmail.com>
2020-09-02 17:44:35 -0300

    refactor: login form

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -1,15 +1,33 @@
 import React from 'react';
 import Styles from './login-styles.scss';
 import { LoginHeader } from '@/presentation/components';
+import Spinner from '@/presentation/components/spinner/spinner';
 
 const Login: React.FC = () => (
   <div className={Styles.loginWrap}>
     <LoginHeader />
     <form data-testid='form' className={Styles.form}>
       <h2>Login</h2>
-      <input type='email' name='email' placeholder='Digite seu e-mail' />
-      <input type='password' name='password' placeholder='Digite sua senha' />
+      <div className={Styles.inputWrap}>
+        <input type='email' name='email' placeholder='email@email.com' />
+        <span className={Styles.status}>🔴</span>
+      </div>
+      <div className={Styles.inputWrap}>
+        <input type='password' name='password' placeholder='Password' />
+        <span className={Styles.status}>🔴</span>
+      </div>
+      <button className={Styles.submit} type='submit'>
+        Enter
+      </button>
+      <span className={Styles.link}>Create Account</span>
+      <div className={Styles.errorWrap}>
+        <Spinner className={Styles.spinner} />
+        <span className={Styles.error}>Error</span>
+      </div>
     </form>
+    <footer>
+      <h2>Login</h2>
+    </footer>
   </div>
 );
 
