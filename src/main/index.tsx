c1c17f74c8f9ff87735be47872df27525f68a51b
Isaque Igor da Silva <isaqueigorsilva@gmail.com>
2020-09-03 13:50:24 -0300

    fix: move global styles to entry point

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import '@/presentation/styles/global.scss';
 import { Router } from '@/presentation/components';
 
 ReactDOM.render(<Router />, document.getElementById('main'));
