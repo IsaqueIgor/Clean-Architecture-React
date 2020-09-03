d691f9a7ee62458679e7af6c2689e4e549e600c1
Isaque Igor da Silva <isaqueigorsilva@gmail.com>
2020-09-03 17:13:33 -0300

    feat: ensure Login will not render spinner and error on start

--- src/presentation/components/form-status/form-status.tsx
+++ src/presentation/components/form-status/form-status.tsx
@@ -1,13 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Styles from './form-status-styles.scss';
-
+import Context from '@/presentation/contexts/form/form-context';
 import { Spinner } from '..';
 
 const FormStatus: React.FC = () => {
+  const { isLoading, errorMessage } = useContext(Context);
   return (
-    <div className={Styles.errorWrap}>
-      <Spinner className={Styles.spinner} />
-      <span className={Styles.error}>Error</span>
+    <div data-testid='error-wrap' className={Styles.errorWrap}>
+      {isLoading && <Spinner className={Styles.spinner} />}
+      {errorMessage && <span className={Styles.error}>{errorMessage}</span>}
     </div>
   );
 };
--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Styles from './login-styles.scss';
 import {
   LoginHeader,
@@ -7,22 +7,38 @@ import {
   FormStatus,
 } from '@/presentation/components';
 
-const Login: React.FC = () => (
-  <div className={Styles.loginWrap}>
-    <LoginHeader />
-    <form data-testid='form' className={Styles.form}>
-      <h2>Login</h2>
-      <Input type='email' name='email' placeholder='E-mail' />
-      <Input type='password' name='password' placeholder='Password' />
+import Context from '@/presentation/contexts/form/form-context';
 
-      <button className={Styles.submit} type='submit'>
-        Enter
-      </button>
-      <span className={Styles.link}>Create Account</span>
-      <FormStatus />
-    </form>
-    <Footer />
-  </div>
-);
+type StateProps = {
+  isLoading: boolean;
+  errorMessage: string;
+};
+
+const Login: React.FC = () => {
+  const [state] = useState<StateProps>({
+    isLoading: false,
+    errorMessage: '',
+  });
+
+  return (
+    <div className={Styles.loginWrap}>
+      <LoginHeader />
+      <Context.Provider value={state}>
+        <form data-testid='form' className={Styles.form}>
+          <h2>Login</h2>
+          <Input type='email' name='email' placeholder='E-mail' />
+          <Input type='password' name='password' placeholder='Password' />
+
+          <button className={Styles.submit} type='submit'>
+            Enter
+          </button>
+          <span className={Styles.link}>Create Account</span>
+          <FormStatus />
+        </form>
+      </Context.Provider>
+      <Footer />
+    </div>
+  );
+};
 
 export default Login;
