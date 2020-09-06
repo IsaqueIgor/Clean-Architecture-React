16751a9b4f9cc87421d533ba7108ddd635a1f5a3
Isaque Igor da Silva <isaqueigorsilva@gmail.com>
2020-09-06 20:11:39 -0300

    test: ensure Login calls validation with correct password

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -43,12 +43,21 @@ describe('Login Component', () => {
     expect(submitButton.disabled).toBe(true);
   });
 
-  test('Should call Validation with correct value', () => {
+  test('Should call Validation with correct email', () => {
     const { sut, validationSpy } = makeSut();
     const emailInput = sut.getByTestId('email');
     fireEvent.input(emailInput, { target: { value: 'any_email' } });
     expect(validationSpy.input).toEqual({
       email: 'any_email',
+    });
+  });
+
+  test('Should call Validation with correct password', () => {
+    const { sut, validationSpy } = makeSut();
+    const passwordInput = sut.getByTestId('password');
+    fireEvent.input(passwordInput, { target: { value: 'any_password' } });
+    expect(validationSpy.input).toEqual({
+      password: 'any_password',
     });
   });
 });
