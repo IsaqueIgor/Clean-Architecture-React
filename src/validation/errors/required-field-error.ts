34291ac627fe8fdd658ca4d788d3c3abc7456d25
Isaque Igor da Silva <isaqueigorsilva@gmail.com>
2020-09-12 11:51:31 -0300

    feat:ensure requiredFieldValidation returns error if field is empty

---
+++ src/validation/errors/required-field-error.ts
@@ -0,0 +1,6 @@
+export class RequiredFieldError extends Error {
+  constructor() {
+    super('Required Field');
+    this.name = 'RequiredFieldError';
+  }
+}
---
+++ src/validation/required-field/required-field-validation.ts
@@ -0,0 +1,10 @@
+import { FieldValidation } from '../protocols/field-validation';
+import { RequiredFieldError } from '@/validation/errors';
+
+export class RequiredFieldValitation implements FieldValidation {
+  constructor(readonly field: string) {}
+
+  validate(value: string): Error {
+    return new RequiredFieldError();
+  }
+}
