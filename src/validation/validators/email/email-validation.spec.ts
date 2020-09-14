517e5593e2052ffde116e2af4b0696d318408fe9
Isaque Igor da Silva <isaqueigorsilva@gmail.com>
2020-09-14 17:26:54 -0300

    fix: ensure email validation returns erro when email is empty

--- src/validation/validators/email/email-validation.spec.ts
+++ src/validation/validators/email/email-validation.spec.ts
@@ -18,4 +18,10 @@ describe('EmailValidation', () => {
     const error = sut.validate(faker.internet.email());
     expect(error).toBeFalsy();
   });
+
+  test('should return falsy if email is empty', () => {
+    const sut = makeSut();
+    const error = sut.validate('');
+    expect(error).toBeFalsy();
+  });
 });
