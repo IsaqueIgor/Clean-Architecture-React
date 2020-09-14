ed1cd4dab26885660df37bc0b346c24dcc6d7a04
Isaque Igor da Silva <isaqueigorsilva@gmail.com>
2020-09-14 16:43:09 -0300

    test: ensure minLengthValidation returns falsy if value is valid

--- src/validation/validators/min-lenght/min-length-validation.spec.ts
+++ src/validation/validators/min-lenght/min-length-validation.spec.ts
@@ -7,4 +7,10 @@ describe('MinLengthValidation', () => {
     const error = sut.validate('123');
     expect(error).toEqual(new InvalidFieldError());
   });
+
+  test('Should return false if value is valid', () => {
+    const sut = new MinLengthValidation('field', 5);
+    const error = sut.validate('12398');
+    expect(error).toBeFalsy();
+  });
 });
