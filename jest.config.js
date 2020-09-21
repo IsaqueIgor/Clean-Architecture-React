dd9c2c2460662ce4c1adb2e8cfa35044be73d7af
Isaque Igor da Silva <isaqueigorsilva@gmail.com>
2020-09-21 15:42:45 -0300

    chore: test coverage ignore main layer

--- jest.config.js
+++ jest.config.js
@@ -2,15 +2,16 @@ module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: [
     '<rootDir>/src/**/*.{ts,tsx}',
-    '!**/*.d.ts'
+    '!<rootDir>/src/main**/*',
+    '!**/*.d.ts',
   ],
   coverageDirectory: 'coverage',
   testEnvironment: 'jsdom',
   transform: {
-    '.+\\.(ts|tsx)$': 'ts-jest'
+    '.+\\.(ts|tsx)$': 'ts-jest',
   },
   moduleNameMapper: {
     '@/(.*)': '<rootDir>/src/$1',
-    '\\.scss$': 'identity-obj-proxy'
-  }
-}
+    '\\.scss$': 'identity-obj-proxy',
+  },
+};
