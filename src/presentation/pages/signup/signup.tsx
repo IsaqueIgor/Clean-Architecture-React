0201eaab2ea09a3614a67eb9165bb0074806dce8
Isaque Igor da Silva <isaqueigorsilva@gmail.com>
2020-10-03 11:56:04 -0300

    feat: add states to signup page

--- src/presentation/pages/signup/signup.tsx
+++ src/presentation/pages/signup/signup.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { Link } from 'react-router-dom';
 import {
@@ -11,32 +11,45 @@ import Context from '@/presentation/contexts/form/form-context';
 
 import Styles from './signup-styles.scss';
 
-const SignUp: React.FC = () => (
-  <div className={Styles.signup}>
-    <LoginHeader />
-    <Context.Provider value={{ state: {} }}>
-      <form className={Styles.form}>
-        <h2>Register</h2>
-        <Input type="text" name="name" placeholder="Your Name" />
-        <Input type="email" name="email" placeholder="Email" />
-        <Input type="password" name="password" placeholder="Password" />
-        <Input
-          type="password"
-          name="passwordConfirmation"
-          placeholder="Confirm your Password"
-        />
+const SignUp: React.FC = () => {
+  const [state] = useState({
+    isLoading: false,
+    nameError: 'Required Field',
+    passwordError: 'Required Field',
+    emailError: 'Required Field',
+    passwordConfirmationError: 'Required Field',
+    mainError: '',
+  });
+  return (
+    <div className={Styles.signup}>
+      <LoginHeader />
+      <Context.Provider value={{ state }}>
+        <form className={Styles.form}>
+          <h2>Register</h2>
+          <Input type="text" name="name" placeholder="Your Name" />
+          <Input type="email" name="email" placeholder="Email" />
+          <Input type="password" name="password" placeholder="Password" />
+          <Input
+            type="password"
+            name="passwordConfirmation"
+            placeholder="Confirm your Password"
+          />
 
-        <button className={Styles.submit} type="submit">
-          Sign up
-        </button>
-        <Link to="/login" className={Styles.link}>
-          Back to Login
-        </Link>
-        <FormStatus />
-      </form>
-    </Context.Provider>
-    <Footer />
-  </div>
-);
+          <button
+            data-testid="submit"
+            disabled
+            className={Styles.submit}
+            type="submit"
+          >
+            Sign up
+          </button>
+          <span className={Styles.link}>Back to Login</span>
+          <FormStatus />
+        </form>
+      </Context.Provider>
+      <Footer />
+    </div>
+  );
+};
 
 export default SignUp;
