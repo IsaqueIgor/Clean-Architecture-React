0c19de175b1fc2900975a565a243a075d87d130e
Isaque Igor da Silva <isaqueigorsilva@gmail.com>
2020-10-07 19:54:35 -0300

    test: add SignupValidationFactory

---
+++ src/main/factories/pages/signup/signup-validation-factory.spec.ts
@@ -0,0 +1,21 @@
+import { ValidationComposite } from '@/validation/validators';
+import { ValidationBuilder } from '@/validation/validators/builder/validation-builder';
+
+import { makeSignUpValidation } from './signup-validation-factory';
+
+describe('LoginValidationFactory', () => {
+  test('should make ValidationComposite with correct validations', () => {
+    const composite = makeSignUpValidation();
+    expect(composite).toEqual(
+      ValidationComposite.build([
+        ...ValidationBuilder.field('name').required().min(4).build(),
+        ...ValidationBuilder.field('email').required().email().build(),
+        ...ValidationBuilder.field('password').required().min(5).build(),
+        ...ValidationBuilder.field('passwordConfirmation')
+          .required()
+          .min(5)
+          .build(),
+      ])
+    );
+  });
+});
