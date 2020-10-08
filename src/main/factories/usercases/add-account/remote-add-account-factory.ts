49a6f0f5529bc177003bb5c2f8d7d7a9e161c11c
Isaque Igor da Silva <isaqueigorsilva@gmail.com>
2020-10-08 19:52:34 -0300

    fix: change eslint to parserOption tsconfig lint

--- src/main/factories/usercases/add-account/remote-add-account-factory.ts
+++ src/main/factories/usercases/add-account/remote-add-account-factory.ts
@@ -1,8 +1,6 @@
-/* eslint-disable implicit-arrow-linebreak */
 import { AddAccount } from '@/domain/userCases';
 import { makeApiUrl } from '@/main/factories/http/api-url-factory';
 import { makeAxiosHttpClient } from '@/main/factories/http/axios-http-client-factory';
 import { RemoteAddAccount } from '@/data/userCases/add-account/remote-add-account';
 
-export const makeRemoteAddAccount = (): AddAccount =>
-  new RemoteAddAccount(makeApiUrl('/signup'), makeAxiosHttpClient());
+export const makeRemoteAddAccount = (): AddAccount => new RemoteAddAccount(makeApiUrl('/signup'), makeAxiosHttpClient());
