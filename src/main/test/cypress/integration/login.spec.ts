2637d84d719693a916e6455402752321b0dd586b
Isaque Igor da Silva <isaqueigorsilva@gmail.com>
2020-10-08 20:08:47 -0300

    test: ensure login begin  with correct initial state

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -1,5 +1,20 @@
 describe('Login', () => {
-  it('Sould load ith correct initial state', () => {
+  beforeEach(() => {
     cy.visit('login');
+  });
+
+  it('Should load ith correct initial state', () => {
+    cy.getByTestId('email-status').should(
+      'have.attr',
+      'title',
+      'Required Field'
+    );
+    cy.getByTestId('password-status').should(
+      'have.attr',
+      'title',
+      'Required Field'
+    );
+    cy.getByTestId('submit').should('have.attr', 'disabled');
+    cy.getByTestId('error-wrap').should('not.have.descendants');
   });
 });
