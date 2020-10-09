d357c1b6531232bc385c73d792c13ad42e2dfede
Isaque Igor da Silva <isaqueigorsilva@gmail.com>
2020-10-09 18:54:34 -0300

    test: ensure Login present erro state  if form is invalid

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -1,3 +1,5 @@
+import faker from 'faker';
+
 describe('Login', () => {
   beforeEach(() => {
     cy.visit('login');
@@ -13,6 +15,24 @@ describe('Login', () => {
       'have.attr',
       'title',
       'Required Field'
+    );
+    cy.getByTestId('submit').should('have.attr', 'disabled');
+    cy.getByTestId('error-wrap').should('not.have.descendants');
+  });
+
+  it('Should present error state if form is invalid', () => {
+    cy.getByTestId('email').type(faker.random.word());
+    cy.getByTestId('email-status').should(
+      'have.attr',
+      'title',
+      'Invalid field'
+    );
+    cy.getByTestId('password').type(faker.random.alphaNumeric(3));
+
+    cy.getByTestId('password-status').should(
+      'have.attr',
+      'title',
+      'Invalid field'
     );
     cy.getByTestId('submit').should('have.attr', 'disabled');
     cy.getByTestId('error-wrap').should('not.have.descendants');
