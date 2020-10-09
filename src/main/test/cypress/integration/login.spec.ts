da939f0b2d6b890969557d3d0094bc04f7728511
Isaque Igor da Silva <isaqueigorsilva@gmail.com>
2020-10-09 19:06:31 -0300

    test: ensure Login present error message if invalid credentials are provided

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -1,5 +1,7 @@
 import faker from 'faker';
 
+const { baseUrl } = Cypress.config();
+
 describe('Login', () => {
   beforeEach(() => {
     cy.visit('login');
@@ -50,5 +52,21 @@ describe('Login', () => {
     );
     cy.getByTestId('submit').should('not.have.attr', 'disabled');
     cy.getByTestId('error-wrap').should('not.have.descendants');
+  });
+
+  it('Should present error if invalid credentials are provided', () => {
+    cy.getByTestId('email').type(faker.internet.email());
+    cy.getByTestId('password').type(faker.random.alphaNumeric(5));
+    cy.getByTestId('submit').click();
+    cy.getByTestId('error-wrap')
+      .getByTestId('spinner')
+      .should('exist')
+      .getByTestId('main-error')
+      .should('not.exist')
+      .getByTestId('spinner')
+      .should('not.exist')
+      .getByTestId('main-error')
+      .should('contain.text', 'Invalid Credentials');
+    cy.url().should('eq', `${baseUrl}/login`);
   });
 });
