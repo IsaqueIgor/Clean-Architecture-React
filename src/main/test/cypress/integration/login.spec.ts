4560b0bca92b12cf312ea2deead98c4d0c4f8edc
Isaque Igor da Silva <isaqueigorsilva@gmail.com>
2020-10-09 23:01:49 -0300

    test: ensure Login save accessToken if valid credentials are provided

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -2,6 +2,11 @@ import faker from 'faker';
 
 const { baseUrl } = Cypress.config();
 
+const validCredentials = {
+  email: 'mango@gmail.com',
+  password: '123456',
+};
+
 describe('Login', () => {
   beforeEach(() => {
     cy.visit('login');
@@ -68,5 +73,20 @@ describe('Login', () => {
       .getByTestId('main-error')
       .should('contain.text', 'Invalid Credentials');
     cy.url().should('eq', `${baseUrl}/login`);
+  });
+
+  it('Should present save accessToken if valid credentials are provided', () => {
+    cy.getByTestId('email').type(validCredentials.email);
+    cy.getByTestId('password').type(validCredentials.password);
+    cy.getByTestId('submit').click();
+    cy.getByTestId('error-wrap')
+      .getByTestId('spinner')
+      .should('exist')
+      .getByTestId('main-error')
+      .should('not.exist')
+      .getByTestId('spinner')
+      .should('not.exist');
+    cy.url().should('eq', `${baseUrl}/`);
+    cy.window().then((window) => assert.isOk(window.localStorage.getItem('accessToken')));
   });
 });
