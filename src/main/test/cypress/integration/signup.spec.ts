914dca43634837c7444d7a5731e6ca3ec6c467f5
Isaque Igor da Silva <isaqueigorsilva@gmail.com>
2020-10-14 10:49:36 -0300

    test: ensure Signup page present error state if form is invalid

--- src/main/test/cypress/integration/signup.spec.ts
+++ src/main/test/cypress/integration/signup.spec.ts
@@ -15,4 +15,21 @@ describe('Sign Up', () => {
     cy.getByTestId('submit').should('have.attr', 'disabled');
     cy.getByTestId('error-wrap').should('not.have.descendants');
   });
+
+  it('Should present error state if form is invalid', () => {
+    cy.getByTestId('name').type(faker.random.alphaNumeric(2));
+    FormHelper.testInputStatus('name', 'Invalid field');
+
+    cy.getByTestId('email').type(faker.random.word());
+    FormHelper.testInputStatus('email', 'Invalid field');
+
+    cy.getByTestId('password').type(faker.random.alphaNumeric(3));
+    FormHelper.testInputStatus('password', 'Invalid field');
+
+    cy.getByTestId('passwordConfirmation').type(faker.random.alphaNumeric(4));
+    FormHelper.testInputStatus('passwordConfirmation', 'Invalid field');
+
+    cy.getByTestId('submit').should('have.attr', 'disabled');
+    cy.getByTestId('error-wrap').should('not.have.descendants');
+  });
 });
