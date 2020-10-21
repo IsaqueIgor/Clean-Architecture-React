23ab2b0be16df949c79c6a6f09e2e80d55b2fe68
Isaque Igor da Silva <isaqueigorsilva@gmail.com>
2020-10-21 13:35:13 -0300

    refactor: move Icon to its own file

--- src/presentation/components/index.ts
+++ src/presentation/components/index.ts
@@ -1,6 +1,7 @@
 export { default as Footer } from './footer/footer';
 export { default as Spinner } from './spinner/spinner';
 export { default as Input } from './input/input';
+export { default as Icon, IconName } from './icon/icon';
 export { default as FormStatus } from './form-status/form-status';
 export { default as Router } from './router/router';
 export { default as Header } from './header/header';
