121b3a0f7af755fafed742c30c527fbe9984c7e4
Isaque Igor da Silva <isaqueigorsilva@gmail.com>
2020-10-25 17:40:13 -0300

    feat: ensure SurveyList component calls LoadSurveyList

--- src/presentation/pages/survey-list/survey-list.tsx
+++ src/presentation/pages/survey-list/survey-list.tsx
@@ -1,22 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import { Footer, Header } from '@/presentation/components';
 import { SurveyItemEmpty } from '@/presentation/pages/survey-list/components';
+import { LoadSurveyList } from '@/domain/userCases';
 
 import Styles from './survey-list-styles.scss';
 
-const SurveyList: React.FC = () => (
-  <div className={Styles.surveyListWrap}>
-    <Header />
-    <main className={Styles.contentWrap}>
-      <h2>Surveys</h2>
-      <span>Below are the public polls</span>
-      <ul data-testid="survey-list">
-        <SurveyItemEmpty />
-      </ul>
-    </main>
-    <Footer />
-  </div>
-);
+type Props = {
+  loadSurveyList: LoadSurveyList;
+};
+
+const SurveyList: React.FC<Props> = ({ loadSurveyList }: Props) => {
+  useEffect(() => {
+    (async function () {
+      loadSurveyList.loadAll();
+    }());
+  }, []);
+
+  return (
+    <div className={Styles.surveyListWrap}>
+      <Header />
+      <main className={Styles.contentWrap}>
+        <h2>Surveys</h2>
+        <span>Below are the public polls</span>
+        <ul data-testid="survey-list">
+          <SurveyItemEmpty />
+        </ul>
+      </main>
+      <Footer />
+    </div>
+  );
+};
 
 export default SurveyList;
