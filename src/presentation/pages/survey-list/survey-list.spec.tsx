176107d8debfa9644177f7154aed21ae2e4c8a27
Isaque Igor da Silva <isaqueigorsilva@gmail.com>
2020-10-25 16:39:45 -0300

    test: ensure surveyList renders 4 empty LI on start

---
+++ src/presentation/pages/survey-list/survey-list.spec.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { render, screen } from '@testing-library/react';
+import { SurveyList } from '@/presentation/pages';
+
+const makeSut = (): void => {
+  render(<SurveyList />);
+};
+
+describe('SurveyList Component', () => {
+  test('Should present 4 empty items on start', () => {
+    makeSut();
+    const surveyList = screen.getByTestId('survey-list');
+    expect(surveyList.querySelectorAll('li:empty').length).toBe(4);
+  });
+});
