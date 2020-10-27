91202271c707ee933f42e6a7827ce410c83c96e7
Isaque Igor da Silva <isaqueigorsilva@gmail.com>
2020-10-26 21:47:40 -0300

    feat: ensure SurveyItem load icon and  day correctly

--- src/presentation/pages/survey-list/components/survey-item/survey-item.tsx
+++ src/presentation/pages/survey-list/components/survey-item/survey-item.tsx
@@ -9,27 +9,31 @@ type Props = {
   survey: SurveyModel;
 };
 
-const SurveyItem: React.FC<Props> = ({ survey }: Props) => (
-  <li>
-    <div className={Styles.surveyContent}>
-      <Icon iconName={IconName.thumbUp} />
-      <time>
-        <span data-testid="day" className={Styles.day}>
-          {survey.date.getDate()}
-        </span>
-        <span data-testid="month" className={Styles.month}>
-          {survey.date
-            .toLocaleString('pt-BR', { month: 'short' })
-            .replace('.', '')}
-        </span>
-        <span data-testid="year" className={Styles.year}>
-          {survey.date.getFullYear()}
-        </span>
-      </time>
-      <p data-testid="question">{survey.question}</p>
-    </div>
-    <footer>See Results</footer>
-  </li>
-);
+const SurveyItem: React.FC<Props> = ({ survey }: Props) => {
+  const iconName = survey.didAnswer ? IconName.thumbUp : IconName.thumbDown;
+
+  return (
+    <li>
+      <div className={Styles.surveyContent}>
+        <Icon iconName={iconName} />
+        <time>
+          <span data-testid="day" className={Styles.day}>
+            {survey.date.getDate().toString().padStart(2, '0')}
+          </span>
+          <span data-testid="month" className={Styles.month}>
+            {survey.date
+              .toLocaleString('pt-BR', { month: 'short' })
+              .replace('.', '')}
+          </span>
+          <span data-testid="year" className={Styles.year}>
+            {survey.date.getFullYear()}
+          </span>
+        </time>
+        <p data-testid="question">{survey.question}</p>
+      </div>
+      <footer>See Results</footer>
+    </li>
+  );
+};
 
 export default SurveyItem;
