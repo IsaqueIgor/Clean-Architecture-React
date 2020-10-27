03a9a80eb08abca22e33af604d0bb54c1db2d584
Isaque Igor da Silva <isaqueigorsilva@gmail.com>
2020-10-26 21:38:36 -0300

    feat: ensure SurveyItem load survey data correctly

--- src/presentation/pages/survey-list/components/survey-item/survey-item.tsx
+++ src/presentation/pages/survey-list/components/survey-item/survey-item.tsx
@@ -1,19 +1,32 @@
 import React from 'react';
 
 import { IconName, Icon } from '@/presentation/components';
+import { SurveyModel } from '@/domain/models';
 
 import Styles from './survey-item-styles.scss';
 
-const SurveyItem: React.FC = () => (
+type Props = {
+  survey: SurveyModel;
+};
+
+const SurveyItem: React.FC<Props> = ({ survey }: Props) => (
   <li>
     <div className={Styles.surveyContent}>
       <Icon iconName={IconName.thumbUp} />
       <time>
-        <span className={Styles.day}>22</span>
-        <span className={Styles.month}>03</span>
-        <span className={Styles.year}>2020</span>
+        <span data-testid="day" className={Styles.day}>
+          {survey.date.getDate()}
+        </span>
+        <span data-testid="month" className={Styles.month}>
+          {survey.date
+            .toLocaleString('pt-BR', { month: 'short' })
+            .replace('.', '')}
+        </span>
+        <span data-testid="year" className={Styles.year}>
+          {survey.date.getFullYear()}
+        </span>
       </time>
-      <p>Qual é seu framework web favotiro?</p>
+      <p data-testid="question">{survey.question}</p>
     </div>
     <footer>See Results</footer>
   </li>
