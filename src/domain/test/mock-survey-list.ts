00598f745fa420769262bb352be02fd7ddd38961
Isaque Igor da Silva <isaqueigorsilva@gmail.com>
2020-10-26 21:43:25 -0300

    feat: ensure SurveyItem load survey data corrently

--- src/domain/test/mock-survey-list.ts
+++ src/domain/test/mock-survey-list.ts
@@ -2,24 +2,28 @@ import faker from 'faker';
 
 import { SurveyModel } from '../models';
 
+export const mockSurveyModel = (): SurveyModel => ({
+  id: faker.random.uuid(),
+  question: faker.random.words(),
+  answers: [
+    {
+      image: faker.internet.url(),
+      answer: faker.random.words(4),
+    },
+    {
+      answer: faker.random.words(5),
+    },
+    {
+      image: faker.internet.url(),
+      answer: faker.random.words(6),
+    },
+  ],
+  date: faker.date.recent(),
+  didAnswer: faker.random.boolean(),
+});
+
 export const mockSurveyListModel = (): SurveyModel[] => [
-  {
-    id: faker.random.uuid(),
-    question: faker.random.words(),
-    answers: [
-      {
-        image: faker.internet.url(),
-        answer: faker.random.words(4),
-      },
-      {
-        answer: faker.random.words(5),
-      },
-      {
-        image: faker.internet.url(),
-        answer: faker.random.words(6),
-      },
-    ],
-    date: faker.date.recent(),
-    didAnswer: faker.random.boolean(),
-  },
+  mockSurveyModel(),
+  mockSurveyModel(),
+  mockSurveyModel(),
 ];
---
+++ src/presentation/pages/survey-list/components/survey-item/survey-item.spec.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import { SurveyItem } from '@/presentation/pages/survey-list/components';
+import { render, screen } from '@testing-library/react';
+import { mockSurveyModel } from '@/domain/test';
+import { SurveyModel } from '@/domain/models';
+import { IconName } from '@/presentation/components';
+import { Console } from 'console';
+
+const makeSut = (survey: SurveyModel): void => {
+  render(<SurveyItem survey={survey} />);
+};
+
+describe('SurveyItem Component', () => {
+  test('Should render with correct values', () => {
+    const survey = Object.assign(mockSurveyModel(), {
+      didAnswer: true,
+      date: new Date('2020-11-10T00:00:00'),
+    });
+    makeSut(survey);
+    expect(screen.getByTestId('icon')).toHaveProperty('src', IconName.thumbUp);
+    expect(screen.getByTestId('question')).toHaveTextContent(survey.question);
+    expect(screen.getByTestId('day')).toHaveTextContent('10');
+    expect(screen.getByTestId('month')).toHaveTextContent('nov');
+    expect(screen.getByTestId('year')).toHaveTextContent('2020');
+  });
+});
