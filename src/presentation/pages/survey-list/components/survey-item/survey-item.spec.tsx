8f3f7113dd5980e235bf54fe13a8e56f572214d6
Isaque Igor da Silva <isaqueigorsilva@gmail.com>
2020-10-26 21:48:36 -0300

    test: ensure SurveyItem load icon and  day correctly

--- src/presentation/pages/survey-list/components/survey-item/survey-item.spec.tsx
+++ src/presentation/pages/survey-list/components/survey-item/survey-item.spec.tsx
@@ -24,4 +24,20 @@ describe('SurveyItem Component', () => {
     expect(screen.getByTestId('month')).toHaveTextContent('nov');
     expect(screen.getByTestId('year')).toHaveTextContent('2020');
   });
+
+  test('Should render with correct values', () => {
+    const survey = Object.assign(mockSurveyModel(), {
+      didAnswer: false,
+      date: new Date('2019-05-01T00:00:00'),
+    });
+    makeSut(survey);
+    expect(screen.getByTestId('icon')).toHaveProperty(
+      'src',
+      IconName.thumbDown
+    );
+    expect(screen.getByTestId('question')).toHaveTextContent(survey.question);
+    expect(screen.getByTestId('day')).toHaveTextContent('01');
+    expect(screen.getByTestId('month')).toHaveTextContent('mai');
+    expect(screen.getByTestId('year')).toHaveTextContent('2019');
+  });
 });
