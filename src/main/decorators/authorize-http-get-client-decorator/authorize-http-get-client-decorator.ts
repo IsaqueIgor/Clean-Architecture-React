9675b450127cb59cd414ab183e86c91a59a3c6c1
Isaque Igor <isaqueigorsilva@gmail.com>
2020-10-31 17:23:55 -0300

    feat: ensure AuthorizeHttpGetClientDecorator returns the  same as HttpGetClient

--- src/main/decorators/authorize-http-get-client-decorator/authorize-http-get-client-decorator.ts
+++ src/main/decorators/authorize-http-get-client-decorator/authorize-http-get-client-decorator.ts
@@ -10,9 +10,12 @@ export class AuthorizeHttpGetClientDecorator implements HttpGetClient {
   async get(params: HttpGetParams): Promise<HttpResponse> {
     const account = this.getStorage.get('account');
     if (account?.accessToken) {
-      Object.assign(params, { headers: Object.assign(params.headers || {}, { 'x-access-token': account.accessToken }) });
+      Object.assign(params, {
+        headers: Object.assign(params.headers || {},
+          { 'x-access-token': account.accessToken })
+      });
     }
-    await this.httpGetClient.get(params);
-    return null;
+    const httpResponse = await this.httpGetClient.get(params);
+    return httpResponse;
   }
 }
