783c2aa609dc9d0fa0bf75d19f4f594ff4a2eee4
Isaque Igor <isaqueigorsilva@gmail.com>
2020-11-02 17:48:34 -0300

    feat: ensure logout calls setCurrentAccount with undefined

--- src/presentation/components/header/header.tsx
+++ src/presentation/components/header/header.tsx
@@ -1,19 +1,34 @@
-import React, { memo } from 'react';
+import React, { memo, useContext } from 'react';
+
+import { ApiContext } from '@/presentation/contexts';
+import { useHistory } from 'react-router-dom';
 
 import Styles from './header-styles.scss';
 
-const Header: React.FC = () => (
-  <header className={Styles.headerWrap}>
-    <div className={Styles.headerContent}>
-      <div className={Styles.logoWrap}>
-        <h1>ASKIT</h1>
+const Header: React.FC = () => {
+  const history = useHistory();
+
+  const { setCurrentAccount } = useContext(ApiContext);
+
+  const logout = (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>): void => {
+    event.preventDefault();
+    setCurrentAccount(undefined);
+    history.replace('/login');
+  };
+
+  return (
+    <header className={Styles.headerWrap}>
+      <div className={Styles.headerContent}>
+        <div className={Styles.logoWrap}>
+          <h1>ASKIT</h1>
+        </div>
+        <div className={Styles.logoutWrap}>
+          <span>Isaque</span>
+          <a data-testid="logout" href="/#" onClick={logout}>Logout</a>
+        </div>
       </div>
-      <div className={Styles.logoutWrap}>
-        <span>Isaque</span>
-        <a href="/#">Logout</a>
-      </div>
-    </div>
-  </header>
-);
+    </header>
+  );
+};
 
 export default memo(Header);
